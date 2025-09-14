5fb2a092648ee7bb37c08ab05f57bbe89e3dd4c2
Nicola Worthington <nicolaw@tfb.net>
2025-09-14 19:35:47 +0100

    Fixup to emojis and table hyperlinking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import CircularDiagram from './components/CircularDiagram';
 
 export default function App() {
+  useEffect(() => {
+    document.title = 'Autism Wheel';
+  }, []);
+
   return (
     <div className="min-h-screen bg-background">
       <CircularDiagram />
