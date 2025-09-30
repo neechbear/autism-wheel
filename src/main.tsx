3cfd996a887d747684b19738637cec580359c236
Nicola Worthington <nicolaw@tfb.net>
2025-09-30 12:11:54 +0100

    Colours

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  import "./styles/globals.css";
+  import "./styles/globals-light.css";
+  import "./styles/globals-dark.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
-  
